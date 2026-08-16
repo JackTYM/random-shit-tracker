ee234095a02ea6f322af8f758f03259ea38f05d8
Jackson Yarger <jacksonkyarger@gmail.com>
2026-08-16 16:56:24 -0700

    Scaffold Cloudflare Workers deployment for Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  nitro: {
+    preset: 'cloudflare_module',
+  },
+  runtimeConfig: {
+    r2AccountId: '',
+    r2AccessKeyId: '',
+    r2SecretAccessKey: '',
+    r2Bucket: '',
+    public: {
+      neonBaseUrl: '',
+      neonAuthUrl: '',
+      neonDataApiUrl: '',
+      r2PublicBaseUrl: '',
+    },
+  },
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: 'anonymous' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700&family=Archivo+Black&family=JetBrains+Mono:wght@400;500;700&display=swap',
+        },
+      ],
+    },
+  },
+  css: ['~/assets/css/tokens.css', '~/assets/css/main.css'],
 })

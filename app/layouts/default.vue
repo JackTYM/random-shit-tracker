1261cbd2ce1ab3e3d6455eb25a07121e86ce0bb1
Jackson Yarger <jacksonkyarger@gmail.com>
2026-08-16 17:06:16 -0700

    Add design system tokens and default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="min-height: 100vh; background: var(--color-paper); font-family: var(--font-ui); color: var(--color-navy)">
+    <AppHeader />
+    <slot />
+  </div>
+</template>
